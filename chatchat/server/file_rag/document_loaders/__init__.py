e3d9e8f63621537bf92249fa678c31b7cbfcab54
xtcg <lvxiatao@gmail.com>
2025-03-23 17:41:59 +0800

    remove all ocr analysis

--- chatchat/server/file_rag/document_loaders/__init__.py
+++ chatchat/server/file_rag/document_loaders/__init__.py
@@ -1,4 +1,4 @@
 from .mydocloader import RapidOCRDocLoader
-from .myimgloader import RapidOCRLoader
-from .mypdfloader import RapidOCRPDFLoader
-from .mypptloader import RapidOCRPPTLoader
+# from .myimgloader import RapidOCRLoader
+# from .mypdfloader import RapidOCRPDFLoader
+# from .mypptloader import RapidOCRPPTLoader
